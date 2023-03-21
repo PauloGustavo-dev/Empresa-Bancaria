6dc4d19c568c92daa42cd6c7c54bad28e750ae61
Paulo Gustavo Sousa Brito <oluap077@gmail.com>
2023-03-20 22:54:15 -0300

    Encapsulando atributos

--- conta.py
+++ conta.py
@@ -1,16 +1,16 @@
 class Conta:
     def __init__(self,numero,titular,saldo,limite):
         print("Construindo objeto ...")
-        self.numero=numero
-        self.titular=titular
-        self.saldo=saldo
-        self.limite=limite
+        self.__numero=numero
+        self.__titular=titular
+        self.__saldo=saldo
+        self.__limite=limite
     
     def extrato(self):
-        print("O saldo do titular {} é {}".format(self.titular,self.saldo))
+        print("O saldo do titular {} é {}".format(self.__titular,self.__saldo))
 
     def deposita(self,valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self,valor):
-        self.saldo -= valor
+        self.__saldo -= valor
